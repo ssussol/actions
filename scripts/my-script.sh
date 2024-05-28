c66c786cd2749d5d5ef12075cd3d74f956b7ecdd
maibine <adi.gheorghica@gmail.com>
2024-05-28 16:47:16 +0300

    added files

---
+++ scripts/my-script.sh
@@ -0,0 +1,6 @@
+# https://docs.github.com/en/enterprise-cloud@latest/actions/learn-github-actions/essential-features-of-github-actions
+search_dir=..
+for entry in "$search_dir"/*
+do
+  echo "$entry"
+done
