70df84d8d29ecc725ec88cb1cadc3c0779653963
maibine <adi.gheorghica@gmail.com>
2024-06-05 12:47:12 +0300

    ss

--- scripts/copytest.sh
+++ scripts/copytest.sh
@@ -11,4 +11,14 @@ DEST_DIR="../dir3"
 mkdir -p "$DEST_DIR"
 
 # Copy .md and .mdx files from source to destination if they do not exist in the destination
-find "$SOURCE_DIR" -type f \( -name "*.md" -o -name "*.mdx" \) -exec rsync -a --ignore-existing {} "$DEST_DIR/" \;
+#find "$SOURCE_DIR" -type f \( -name "*.md" -o -name "*.mdx" \) -exec rsync -a --ignore-existing {} "$DEST_DIR/" \;
+
+# Copy .md and .mdx files from source to destination if they do not exist in the destination
+# and maintain the directory structure
+find "$SOURCE_DIR" -type f \( -name "*.md" -o -name "*.mdx" \) -exec sh -c '
+  for file do
+    dest="$DEST_DIR/${file#$SOURCE_DIR/}"
+    mkdir -p "$(dirname "$dest")"
+    rsync -a --ignore-existing "$file" "$dest"
+  done
+' sh {} +
